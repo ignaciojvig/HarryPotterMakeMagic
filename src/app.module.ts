16a17d659da5bd65c6b6c51196ed7ebd9c36a3c2
Joao Victor Ignacio <joao.ignacio@venturus.org.br>
2020-09-18 16:17:38 -0300

    Applied Cache Manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ControllersModule } from './api/1 - controllers/controllers.module';
 import { SeedingModule } from './environment/database-seeding/seeding.module';
 import { DatabaseModule } from './environment/database/database.module';
 
 @Module({
   imports: [
+    CacheModule.register({
+      ttl: 10,
+      max: 10,
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     SeedingModule,
     ControllersModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule {}
